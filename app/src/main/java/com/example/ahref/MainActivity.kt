91ba55f55cd0933421c8a0cb70f23a5a3fdb4721
Ping Yang <pingyang.tan@bandlab.com>
2024-02-19 17:34:47 +0800

    Added activity

--- app/src/main/java/com/example/ahref/MainActivity.kt
+++ app/src/main/java/com/example/ahref/MainActivity.kt
@@ -1,26 +1,16 @@
 package com.example.ahref
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ahref.ui.theme.AhrefTheme
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AhrefTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    SearchScreen()
-                }
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.fragment_container_view, SearchFragment::class.java, null)
             }
         }
     }
